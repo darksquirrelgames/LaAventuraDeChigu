02d10819af14580d494e37874b78b962fcb3e613
Darkcarard <darkcarard@localhost.localdomain>
2017-02-10 19:04:23 -0500

    Commit inicial del proyecto. Se adicionó el sprite temporal de Chigu y se crearon las clases personaje y jugador. Se adicionaron las funcionalidades de moverse, saltar y girar al jugador

---
+++ La Aventura de Chigu/Assets/Scripts/Jugador.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Jugador : Personaje
+{
+    [SerializeField] protected float fuerzaSalto;
+    Rigidbody2D miRigidBody;
+    bool saltar;
+    public override void Start()
+    {
+        base.Start();
+        saltar = false;
+        miRigidBody = transform.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		 Teclado();
+    }
+    void FixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+       
+        Girar(movimientoHorizontal);
+        Movimiento(movimientoHorizontal);
+    }
+
+    void Girar(float movimientoHorizontal)
+    {
+        if (movimientoHorizontal > 0 && !mirandoDerecha || movimientoHorizontal < 0 && mirandoDerecha)
+        {
+            CambiarDireccion();
+        }
+    }
+
+    void Teclado()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            saltar = true;
+        }
+    }
+
+    void Movimiento(float movimientoHorizontal)
+    {
+        if (saltar)
+        {
+            miRigidBody.AddForce(new Vector2(0, fuerzaSalto));
+			saltar = false;
+        }
+		miRigidBody.velocity = new Vector2(movimientoHorizontal * velocidadMovimiento, miRigidBody.velocity.y);
+    }
+}
---
+++ La Aventura de Chigu/Assets/Scripts/Personaje.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class Personaje : MonoBehaviour {
+
+	[Header ("Configuración del Personaje")]
+	[SerializeField] protected float velocidadMovimiento;
+	
+	protected bool mirandoDerecha;
+
+	public virtual void Start () {
+		mirandoDerecha = true;
+	}
+	
+	public void CambiarDireccion () {
+		mirandoDerecha = !mirandoDerecha;
+		transform.localScale = new Vector3 (transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
+	}
+}
