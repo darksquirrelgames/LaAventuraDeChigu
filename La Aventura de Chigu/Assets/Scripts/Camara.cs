259b2a8070c9a391ff036079d0c105ec6e93d8fe
Darkcarard <darksquirrelgames@gmail.com>
2017-02-14 18:36:14 -0500

    Nuevos fondos y personaje en pixel art. Animación de caminar para Chigu. Ajustes del parralax de los fondos

---
+++ La Aventura de Chigu/Assets/Scripts/Camara.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+
+	[SerializeField] Transform objetivo;
+	[SerializeField] float offset = 2f;
+	[SerializeField] float suavidadMovimiento = 1f;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 position = transform.position;
+        position.x = objetivo.transform.position.x + offset;
+		transform.position = Vector3.Lerp(transform.position, position, suavidadMovimiento*Time.deltaTime);
+    }
+}
